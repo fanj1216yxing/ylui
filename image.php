8fd33e9d0c1161775e68526f25e5ae85077607a3
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2022-06-01 20:56:14 +0800

    Create image.php

---
+++ image.php
@@ -0,0 +1,36 @@
+
+<?php
+
+$arr = array(
+
+'https://api.ixiaowai.cn/api/api.php',
+
+'http://www.dmoe.cc/random.php',
+
+'https://api.ixiaowai.cn/api/api.php',
+
+'https://www.yingciyuan.cn/api.php',
+
+'https://api.ghser.com/random/api.php',
+
+'https://tuapi.eees.cc/api.php?category={dongman,fengjing}',
+
+'https://tenapi.cn/acg',
+
+'https://api.qicaiyun.top/ercy/api.php',
+
+'https://api.likepoems.com/pc',
+
+'http://api.x-xh.cn/xjg/z.php'
+
+);
+
+$key = array_rand($arr, 1);
+
+//输出随机内容
+
+// echo $arr[$key];
+
+header('Location: ' . $arr[$key]);
+
+exit;
